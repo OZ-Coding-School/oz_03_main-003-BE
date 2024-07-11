f135cd21e6f869f905a4a05f636bea17d1739cbc
Sullung <sullung2yo@gmail.com>
2024-07-11 14:15:08 +0900

    Private RDS connection test

--- api/ett_backend/config/settings.py
+++ api/ett_backend/config/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
