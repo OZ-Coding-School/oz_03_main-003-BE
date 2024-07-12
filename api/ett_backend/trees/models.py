12cb5be164955fe125bff0b694dc053dd658225e
Scanf-s <calzone0404@gmail.com>
2024-07-12 14:41:44 +0900

    Refactor: refactored models, Feat: Add health check view

--- api/ett_backend/trees/models.py
+++ api/ett_backend/trees/models.py
@@ -7,13 +7,13 @@ from users.models import User
 
 
 class TreeMap(TimeStampModel):
-    tree_map_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    tree_map_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     tree_level = models.PositiveIntegerField(default=0)
 
 
 class TreeDetail(TimeStampModel):
-    tree_name = models.CharField(max_length=255, default="")
+    tree_name = models.CharField(max_length=255, default="My Tree")
     tree_map = models.ForeignKey(TreeMap, on_delete=models.CASCADE)
     tree_growth_level = models.PositiveIntegerField(default=0)
     location_x = models.IntegerField(default=0)  # x coordinate
@@ -22,4 +22,3 @@ class TreeDetail(TimeStampModel):
     anger = models.PositiveIntegerField(default=0) # 화남
     sadness = models.PositiveIntegerField(default=0) # 슬픔
     pleasure = models.PositiveIntegerField(default=0) # 기쁨
-    indifference = models.PositiveIntegerField(default=0) # 무관심
