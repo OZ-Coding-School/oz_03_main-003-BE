cec733724de97466b48bc376599d7172220f7c48
ChoiSeongRak <kok12346@gmail.com>
2024-07-15 18:45:51 +0900

    add trees/view.py

--- api/ett_backend/config/urls.py
+++ api/ett_backend/config/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/health/", include("common.urls")),
+    path("api/chat/", include("dialog.urls")),
 ]
