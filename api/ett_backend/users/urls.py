e60256a06d969d0ed3c72bca919a73f6597deb81
Scanf-s <calzone0404@gmail.com>
2024-07-17 17:34:09 +0900

    Fix

--- api/ett_backend/users/urls.py
+++ api/ett_backend/users/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 
-from users.views.google_auth_view import UserGoogleLoginCallbackView, UserGoogleLoginView
+from users.views.google_auth_view import UserGoogleTokenReceiver
 # from users.views.user_auth_view import UserLogoutView, UserDeleteView
 
 urlpatterns = [
-    path("google/login", UserGoogleLoginView.as_view(), name="google_login"),
-    path("google/callback", UserGoogleLoginCallbackView.as_view(), name="google_callback"),
-    # path("logout", UserLogoutView.as_view(), name="logout"),
-    # path("delete", UserDeleteView.as_view(), name="user_delete"),
-    # path("profile", UserProfileView.as_view()),
-    # path("refresh", CustomUserTokenRefreshView.as_view()),
+    path("google/receiver", UserGoogleTokenReceiver.as_view(), name="google_receiver"),
 ]
