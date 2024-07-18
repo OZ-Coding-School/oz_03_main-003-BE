a01fa3aeba4b6a4dfe9b4a67ace25f1cbc03699e
ChoiSeongRak <kok12346@gmail.com>
2024-07-18 15:13:43 +0900

    Add App Forest

--- api/ett_backend/trees/views.py
+++ api/ett_backend/trees/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,7 +8,7 @@ from .models import TreeDetail, TreeMap
 
 
 class TreeManagementView(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         # 트리 맵 조회
