d22565ab0e86afec3521183330b477cdc95b1fca
Scanf-s <calzone0404@gmail.com>
2024-07-23 15:51:16 +0900

    Fix API Problems

--- api/ett_backend/trees/urls.py
+++ api/ett_backend/trees/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("new", TreeCreateView.as_view(), name="tree_create_view"),
     path("", TreeListView.as_view(), name="tree_list_retrieve_view"),
     path("<uuid:tree_uuid>", TreeUpdateDeleteView.as_view(), name="tree_update_delete_view"),
-    path("emotion/", TreeEmotionListView.as_view(), name="tree_emotion_view"),
+    path("emotion", TreeEmotionListView.as_view(), name="tree_emotion_view"),
 ]
